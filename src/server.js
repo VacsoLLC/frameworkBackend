dc611b303abf42ae1a58238136dd498bf848753d
Tripp Donnelly <tripp@wasteoftime.org>
2024-08-11 19:52:11 -0500

    handle shutdown

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import http from 'http';
 import express from 'express';
 import router from './lib/router.js';
 
-
 export default class Backend {
   constructor(config) {
     this.config = {
@@ -14,16 +13,32 @@ export default class Backend {
   async start() {
     const { PORT = 3001 } = this.config;
     const app = express();
-    const httpServer = http.createServer(app);
+    this.server = http.createServer(app);
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use('/api', await router(this.config));
 
-    httpServer.listen(PORT, () => {
+    this.server.listen(PORT, () => {
       console.log(`Server listening at http://localhost:${PORT}`);
     });
+
+    // Handle SIGTERM signal
+    process.on('SIGTERM', this.stop);
+
+    // Handle SIGINT signal (Ctrl+C)
+    process.on('SIGINT', this.stop);
+  }
+
+  stop() {
+    console.log('Shutting down gracefully...');
+    if (this.server) {
+      this.server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+      });
+    } else {
+      process.exit(0);
+    }
   }
 }
-
-
